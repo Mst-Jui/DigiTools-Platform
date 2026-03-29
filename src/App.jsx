d0718bd8f9bb925ad080cffd691a5560e273b458
Mst-Jui <contact.juiakter@gmail.com>
2026-03-30 03:43:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+import Banner from './components/Banner'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+    <header className='max-w-6xl mx-auto'>
+      <Navbar/>
+    </header>
+    <hr className='text-[#F2F2F2]' />
+
+    {/* <Banner/> */}
+
+    
+    
+
+    
+    
+
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { FiShoppingCart } from 'react-icons/fi';
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+          </div>
+          {/* <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li><a>Item 1</a></li>
+            <li>
+              <a>Parent</a>
+              <ul className="p-2">
+                <li><a>Submenu 1</a></li>
+                <li><a>Submenu 2</a></li>
+              </ul>
+            </li>
+            <li><a>Item 3</a></li>
+          </ul> */}
+        </div>
+        <a className="font-extrabold text-4xl bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent">DigiTools</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 font-semibold">
+          <li><a>Products</a></li>
+          {/* <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="p-2 bg-base-100 w-40 z-1">
+                <li><a>Submenu 1</a></li>
+                <li><a>Submenu 2</a></li>
+              </ul>
+            </details>
+          </li> */}
+          <li><a>Features</a></li>
+          <li><a>Pricing</a></li>
+          <li><a>Testimonials</a></li>
+          <li><a>FAQ</a></li>
+        </ul>
+      </div>
+      <div className="navbar-end flex gap-4">
+        <a className="relative flex items-center justify-center w-10 h-10 rounded-full hover:bg-gray-100 transition">
+
+          {/* Badge */}
+          <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full shadow">
+            0
+          </span>
+
+          {/* Icon */}
+          <FiShoppingCart className="text-2xl text-gray-700" />
+        </a>
+       
+        <a className='font-semibold'>Login</a>
+        <a className="btn font-semibold bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-full">Get Started</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
