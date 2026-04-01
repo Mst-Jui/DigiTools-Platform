32fec717aa5d44e0b71d5ae787815b0944227a15
Mst-Jui <contact.juiakter@gmail.com>
2026-04-01 20:51:44 +0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,62 +1,41 @@
 import React from 'react';
 import { FiShoppingCart } from 'react-icons/fi';
 
-const Navbar = ({cart}) => {
+const Navbar = ({ cart }) => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar px-4 md:px-8 bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <div tabIndex={0}
+            role="button"
+            className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
           </div>
-          {/* <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li><a>Item 1</a></li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
-          </ul> */}
         </div>
-        <a className="font-extrabold text-4xl bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent">DigiTools</a>
+        <a className="font-extrabold text-2xl sm:text-3xl md:text-4xl bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent">DigiTools</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 font-semibold">
+        <ul className="menu menu-horizontal px-1 font-semibold gap-2">
           <li><a>Products</a></li>
-          {/* <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100 w-40 z-1">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </details>
-          </li> */}
           <li><a>Features</a></li>
           <li><a>Pricing</a></li>
           <li><a>Testimonials</a></li>
           <li><a>FAQ</a></li>
         </ul>
       </div>
-      <div className="navbar-end flex gap-4">
-        <a className="relative flex items-center justify-center w-10 h-10 rounded-full hover:bg-gray-100 transition">
+      <div className="navbar-end flex items-center gap-2 sm:gap-4">
+        <a className="relative flex items-center justify-center w-9 h-9 sm:w-10 sm:h-10 text-xl sm:text-2xl rounded-full hover:bg-gray-100 transition">
 
           {/* Badge */}
-          <span className="absolute top-0 right-0 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full shadow">
+          <span className="absolute -top-1 -right-1 w-4 h-4 sm:w-5 sm:h-5 bg-red-500 text-white text-xs font-bold flex items-center justify-center rounded-full shadow">
             {cart.length}
           </span>
 
           {/* Icon */}
           <FiShoppingCart className="text-2xl text-gray-700" />
         </a>
-       
-        <a className='font-semibold'>Login</a>
-        <a className="btn font-semibold bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-full">Get Started</a>
+        <a className='text-sm sm:text-base md:text-lg font-semibold whitespace-nowrap'>Login</a>
+        <a className="btn text-xs sm:text-sm md:text-base px-3 sm:px-5 font-semibold bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-full">Get Started</a>
       </div>
     </div>
   );
